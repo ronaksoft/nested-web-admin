e5c832c79d9b198f642fbae3ba82dfbab9f4d8af
Ali <me@robzizo.ir>
2017-05-11 16:16:46 +0430

    User avatar Component

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Input, Row, Col, Button } from 'antd';
+import UserAvatar from './../avatar/index';
+
 const Search = Input.Search;
 
 
@@ -26,10 +28,11 @@ class Header extends React.Component<IHeaderProps, IHeaderState> {
                     <Col span={14}>
                         <Row type='flex' justify='end'>
                             <Col>
-                                <Button type='toolkit' shape='circle'  icon='notification'></Button>
-                                <Button type='toolkit' shape='circle'  icon='setting'></Button>
-                                <Button type='toolkit' shape='circle'  icon='lock'></Button>
-                                <Button type='toolkit' shape='circle'  icon='logout'></Button>
+                                <Button type='toolkit' shape='circle' icon='notification'></Button>
+                                <Button type='toolkit' shape='circle' icon='setting'></Button>
+                                <Button type='toolkit' shape='circle' icon='lock'></Button>
+                                <Button type='toolkit' shape='circle' icon='logout'></Button>
+                                <UserAvatar size='24' user='' />
                             </Col>
                         </Row>
                     </Col>
---
+++ src/app/components/avatar/index.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import IUser from './../../api/account/interfaces/IUser';
+
+export interface IAvatarProps {
+    borderRadius : string;
+    user : IUser;
+    size : any;
+    style: string;
+    className: string;
+    avatar : boolean;
+    name : boolean;
+    id : boolean;
+}
+
+export interface IAvatarStates {}
+
+class UserAvatar extends React.Component<IAvatarProps, IAvatarStates> {
+  render() {
+    let {
+      borderRadius= '100%',
+      user,
+      size,
+      style,
+      className,
+      avatar,
+      name,
+      id
+    } = this.props;
+
+    size = size.toString(10) + 'px';
+
+    const imageStyle = {
+      display: 'block',
+      borderRadius
+    };
+
+    const innerStyle = {
+      lineHeight: size,
+      textAlign: 'center',
+      borderRadius
+    };
+
+    if (size) {
+      imageStyle.width = size;
+      imageStyle.height = size;
+    }
+
+    let inner, imgDOM, nameDOM, idDOM, classes = [className, 'UserAvatar'];
+
+    if (avatar) {
+      if (user.picture) {
+        imgDOM = <img className='UserAvatar--img' style={imageStyle} src={user.picture}  alt={user.name} />;
+      } else {
+        // iTODO Initails
+      }
+    }
+
+    if ( name ) {
+      nameDOM = <span>{user.name}</span>;
+    }
+
+    if ( id ) {
+      idDOM = <span>{user._id}</span>;
+    }
+
+    inner = imgDOM + nameDOM + idDOM;
+
+
+
+
+    return (
+      <div aria-label={name} className={classes.join(' ')} style={style}>
+        <div className='UserAvatar--inner' style={innerStyle}>
+          {inner}
+        </div>
+      </div>
+    );
+  }
+}
+export default UserAvatar;
