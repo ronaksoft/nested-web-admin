739bae16bb567ff6e0901bfeb03845d1baee2169
Sina <e.hosseini.ir@gmail.com>
2017-05-11 15:02:17 +0430

    add _cver and _cid

--- src/app/services/classes/server/interfaces/ISocketRequest.tsx
+++ src/app/services/classes/server/interfaces/ISocketRequest.tsx
@@ -3,6 +3,8 @@ import IRequest from './IRequest';
 interface ISocketRequest extends IRequest {
     _sk: string | null;
     _ss: string | null;
+    _cver: number;
+    _cid: string;
 }
 
 export default ISocketRequest;
