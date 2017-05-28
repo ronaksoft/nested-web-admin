6fe15ba31022cc44109d45c6da355288c3334ae5
Sina <e.hosseini.ir@gmail.com>
2017-05-28 18:33:50 +0430

    fix filter problem

--- src/app/scenes/Places/index.tsx
+++ src/app/scenes/Places/index.tsx
@@ -14,108 +14,109 @@ export interface IAccountsProps {
 }
 
 export interface IAccountsState {
-  counters: IGetSystemCountersResponse;
-  loadCounters: boolean;
-  selectedFilter: string;
+    counters: IGetSystemCountersResponse;
+    loadCounters: boolean;
+    selectedFilter: string;
 }
 
 class Accounts extends React.Component<IAccountsProps, IAccountsState> {
-  constructor(props: IAccountsProps) {
-    super(props);
-    this.state = {
-      selectedFilter: CPlaceFilterTypes.GRAND_PLACES,
-      counters: {},
-      loadCounters: false,
-    };
-  }
+    constructor(props: IAccountsProps) {
+        super(props);
+        this.state = {
+            selectedFilter: CPlaceFilterTypes.GRAND_PLACES,
+            counters: {},
+            loadCounters: false,
+        };
+    }
 
-  componentDidMount() {
-    let systemApi = new SystemApi();
-    systemApi.getSystemCounters()
-      .then((data: IGetSystemCountersResponse) => {
+    componentDidMount() {
+        let systemApi = new SystemApi();
+        systemApi.getSystemCounters()
+            .then((data: IGetSystemCountersResponse) => {
+                this.setState({
+                    counters: data,
+                    loadCounters: true
+                });
+            });
+    }
+
+    changeFilter(key: string) {
         this.setState({
-          counters: data,
-          loadCounters: true
+            selectedFilter: key,
         });
-      });
-  }
+    }
 
-  changeFilter(key: string) {
-    this.setState({
-      selectedFilter: key,
-    });
-  }
+    render() {
 
-  render() {
-
-    const filterItems = [
-      {
-        key: CPlaceFilterTypes.ALL,
-        name: 'Total',
-        count: this.state.counters.grand_places + this.state.counters.locked_places + this.state.counters.unlocked_places + this.state.counters.personal_places,
-        disableChart: true,
-      },
-      {
-        key: CPlaceFilterTypes.GRAND_PLACES,
-        name: 'Grand Places',
-        count: this.state.counters.grand_places,
-        chartColor: '#00B45A',
-        bgChartColor: '#CBEFDD',
-      },
-      {
-        key: CPlaceFilterTypes.UNLOCKED_PLACES,
-        name: 'Common Places',
-        count: this.state.counters.unlocked_places,
-        chartColor: '#3296FF',
-        bgChartColor: '#D9EBFF',
-      },
-      {
-        key: CPlaceFilterTypes.LOCKED_PLACES,
-        name: 'Private Places',
-        count: this.state.counters.locked_places,
-        chartColor: '#FF6464',
-        bgChartColor: '#FFDFDF',
-      },
-      {
-        key: CPlaceFilterTypes.PERSONAL_PLACES,
-        name: 'Personal Places',
-        count: this.state.counters.personal_places,
-        chartColor: '#FFB300',
-        bgChartColor: '#FFECB3',
-      }
-    ];
-
-    return (
-      <div>
-        <Row className='toolbar' type='flex' align='center'>
-          <Col span={6}>
-            {this.state.loadCounters &&
-            <Filter totalCount={filterItems[0].count} menus={filterItems} onChange={this.changeFilter.bind(this)}/>
+        const filterItems = [
+            {
+                key: CPlaceFilterTypes.ALL,
+                name: 'Total',
+                count: this.state.counters.grand_places + this.state.counters.locked_places + this.state.counters.unlocked_places + this.state.counters.personal_places,
+                disableChart: true,
+            },
+            {
+                key: CPlaceFilterTypes.GRAND_PLACES,
+                name: 'Grand Places',
+                count: this.state.counters.grand_places,
+                chartColor: '#00B45A',
+                bgChartColor: '#CBEFDD',
+            },
+            {
+                key: CPlaceFilterTypes.UNLOCKED_PLACES,
+                name: 'Common Places',
+                count: this.state.counters.unlocked_places,
+                chartColor: '#3296FF',
+                bgChartColor: '#D9EBFF',
+            },
+            {
+                key: CPlaceFilterTypes.LOCKED_PLACES,
+                name: 'Private Places',
+                count: this.state.counters.locked_places,
+                chartColor: '#FF6464',
+                bgChartColor: '#FFDFDF',
+            },
+            {
+                key: CPlaceFilterTypes.PERSONAL_PLACES,
+                name: 'Personal Places',
+                count: this.state.counters.personal_places,
+                chartColor: '#FFB300',
+                bgChartColor: '#FFECB3',
             }
-          </Col>
-          <Col span={18}></Col>
-        </Row>
-        <Row>
-          <Col span={24}>
-            {this.state.loadCounters &&
-            <PlaceList counters={this.state.counters} selectedFilter={this.state.selectedFilter}/>
-            }
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+        ];
+
+        return (
+            <div>
+                <Row className='toolbar' type='flex' align='center'>
+                    <Col span={6}>
+                        {this.state.loadCounters &&
+                        <Filter totalCount={filterItems[0].count} menus={filterItems}
+                                onChange={this.changeFilter.bind(this)}/>
+                        }
+                    </Col>
+                    <Col span={18}></Col>
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        {this.state.loadCounters &&
+                        <PlaceList counters={this.state.counters} selectedFilter={this.state.selectedFilter}/>
+                        }
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 function mapStateToProps(state: any) {
-  return {};
+    return {};
 }
 
 function mapDispatchToProps(dispatch: IDispatch) {
-  return {};
+    return {};
 }
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Accounts);
