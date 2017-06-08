d0f8f29d80223e4990cf96a464835a16aa6e3a82
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2017-06-08 15:35:35 +0430

    Add Health CHeck to Assistant page

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -52,6 +52,9 @@ class Sidebar extends React.Component<IHeaderProps, IHeaderState> {
                     <Menu.Item key='/charts'>
                         <Link to='/charts' activeClassName='active'><Icon type='area-chart' />Charts</Link>
                     </Menu.Item>
+                    <Menu.Item key='/assistant'>
+                        <Link to='/assistant' activeClassName='active'><Icon type='medicine-box' />Assistant</Link>
+                    </Menu.Item>
                 </Menu>
             </div>
         );
