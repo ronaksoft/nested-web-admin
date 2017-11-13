d88cd31db20479c9f43ce0b5e98f77bdf592afcd
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-11-13 11:25:26 +0330

    fix

--- src/app.config.tsx
+++ src/app.config.tsx
@@ -12,17 +12,17 @@ export default {
     },
     'SIGN_OUT_TARGET': 'https://nested.me',
     'WEBSOCKET': {
-        'URL': 'ws://cyrus.ronaksoftware.com:81',
+        'URL': 'wss://webapp.ronaksoftware.com:81',
         'TIMEOUT': 60000,
         'REQUEST_MAX_RETRY_TIMES': 16
     },
     'STORE': {
-        'URL': 'http://xerxes.ronaksoftware.com:83',
+        'URL': 'https://webapp.ronaksoftware.com:83',
         'TOKEN_EXPMS': 3550000
     },
     'REGISTER': {
         'AJAX': {
-            'URL': 'http://cyrus.ronaksoftware.com:82'
+            'URL': 'https://webapp.ronaksoftware.com:81'
         }
     },
     'GRAND_PLACE_REGEX': /^[a-zA-Z][a-zA-Z0-9-]{3,30}[a-zA-Z0-9]$/,
