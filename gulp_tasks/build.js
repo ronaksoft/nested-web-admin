e3ee8fe5b90b126e58ca5b6a7de401db2fc0a0e3
Hamidreza Kazemi <hamidrezakks@gmail.com>
2017-11-21 11:22:40 +0330

    nginx

--- gulp_tasks/build.js
+++ gulp_tasks/build.js
@@ -34,5 +34,5 @@ function build() {
     .pipe(htmlFilter)
     .pipe(htmlmin())
     .pipe(htmlFilter.restore)
-    .pipe(gulp.dest(conf.path.dist()));
+    .pipe(gulp.dest(conf.path.dist('/admin/')));
 }
